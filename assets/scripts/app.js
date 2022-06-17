cb204d05c614d8818fc7ba6e5afdfb25faf70b95
Suhas <kashyap.suhas@protonmail.com>
2022-06-18 01:56:37 +0530

    Project-2: Added Battle log

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,6 +5,11 @@ const HEAL_VALUE = 15;
 
 const MODE_ATTACK = 'ATTACK';
 const MODE_STRONG_ATTACK = 'STRONG_ATTACK';
+const LOG_EVENT_USER_ATTACK = 'USER_ATTACK';
+const LOG_EVENT_STRONG_USER_ATTACK = 'STRONG_USER_ATTACK';
+const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
+const LOG_EVENT_HEAL_USER = 'HEAL_USER';
+const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
 const enteredValue = prompt('Enter the max life of You and the Monster', '100');
 
@@ -17,8 +22,55 @@ if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
 let currentMonsterHealth = chosenMaxLife;
 let currentUserHealth = chosenMaxLife;
 let hasBonusLife = true;
+let battleLog = [];
 
 adjustHealthBars(chosenMaxLife);
+
+function writeLog (ev, val, userHealth, monsterHealth) {
+  let logEntry;
+  if (ev === LOG_EVENT_USER_ATTACK) {
+    logEntry = {
+      event: ev,
+      value: val,
+      target: 'MONSTER',
+      finalUserHealth: userHealth,
+      finalMonsterHealth: monsterHealth,
+    };
+  } else if (ev === LOG_EVENT_STRONG_USER_ATTACK) {
+    logEntry = {
+      event: ev,
+      value: val,
+      target: 'MONSTER',
+      finalUserHealth: userHealth,
+      finalMonsterHealth: monsterHealth,
+    };
+  } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
+    logEntry = {
+      event: ev,
+      value: val,
+      target: 'USER',
+      finalUserHealth: userHealth,
+      finalMonsterHealth: monsterHealth,
+    };
+  } else if (ev === LOG_EVENT_HEAL_USER) {
+    logEntry = {
+      event: ev,
+      value: val,
+      target: 'USER',
+      finalUserHealth: userHealth,
+      finalMonsterHealth: monsterHealth,
+    };
+  } else if (ev === LOG_EVENT_GAME_OVER) {
+    logEntry = {
+      event: ev,
+      value: val,
+      target: 'MONSTER',
+      finalUserHealth: userHealth,
+      finalMonsterHealth: monsterHealth,
+    };
+  }
+  battleLog.push(logEntry);
+}
 
 function reset() {
 	currentMonsterHealth = chosenMaxLife;
@@ -30,6 +82,7 @@ function endRound() {
 	let initialUserHealth = currentUserHealth;
 	const userDamage = dealPlayerDamage(MONSTER_ATTACK);
 	currentUserHealth -= userDamage;
+  writeLog(LOG_EVENT_MONSTER_ATTACK, userDamage, currentUserHealth, currentMonsterHealth);
 
 	if (currentUserHealth <= 0 && hasBonusLife) {
 		hasBonusLife = false;
@@ -40,10 +93,13 @@ function endRound() {
 	}
 	if (currentMonsterHealth <= 0 && currentUserHealth > 0) {
 			alert('You Won!!');
+      writeLog(LOG_EVENT_GAME_OVER, 'User Won', currentUserHealth, currentMonsterHealth);
 	} else if (currentUserHealth <= 0 && currentMonsterHealth > 0) {
 			alert('You Lost :(');
+      writeLog(LOG_EVENT_GAME_OVER, 'Monster Won', currentUserHealth, currentMonsterHealth);
 	} else if (currentMonsterHealth <= 0 && currentUserHealth <= 0) {
 			alert('Its a TIE');
+      writeLog(LOG_EVENT_GAME_OVER, 'Match Tied', currentUserHealth, currentMonsterHealth);
   }
 
   if (currentUserHealth <= 0 || currentMonsterHealth <= 0) {
@@ -53,13 +109,17 @@ function endRound() {
 
 function attackMonster(mode) {
 	let maxDamage;
+  let logEvent;
 	if (mode === MODE_ATTACK) {
 			maxDamage = USER_ATTACK;
+      logEvent = LOG_EVENT_USER_ATTACK;
 	} else if (mode === MODE_STRONG_ATTACK) {
 			maxDamage = STRONG_USER_ATTACK;
+      logEvent = LOG_EVENT_STRONG_USER_ATTACK;
 	}
 	const damage = dealMonsterDamage(maxDamage);
 	currentMonsterHealth -= damage;
+  writeLog(logEvent, damage, currentUserHealth, currentMonsterHealth);
 	endRound();
 }
 
@@ -81,9 +141,15 @@ function healUserHandler() {
 	}
 	increasePlayerHealth(healValue);
 	currentUserHealth += healValue;
+  writeLog(LOG_EVENT_HEAL_USER, healValue, currentUserHealth, currentMonsterHealth);
 	endRound();
+}
+
+function printLogHandler() {
+  console.log(battleLog);
 }
 
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
 healBtn.addEventListener('click', healUserHandler);
+logBtn.addEventListener('click', printLogHandler);
